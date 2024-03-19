1b3faec6fc004c16f74441c841295ba8f16cb580
Vũ Minh Tuấn <tuanvmth1911008@fpt.edu.vn>
2024-03-20 00:30:00 +0700

    update code

--- src/app/pages/partner/customer/customer-list/customer-list.component.ts
+++ src/app/pages/partner/customer/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-partner',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.css'],
 })
 export class CustomerListComponent implements OnInit {
+  title: string = "Danh sách khách hàng";
+  customerID: number = 0;
 
-  constructor() {
+  constructor(
+    private titleService: Title
+  ) {
   }
 
   ngOnInit() {
+    this.titleService.setTitle(this.title);
   }
 }
