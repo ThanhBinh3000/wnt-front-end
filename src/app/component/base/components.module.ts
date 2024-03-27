421ded9bf089ab12cfe934ea4d9df11c3f194e31
giangnt <giangnt@tecapro.com.vn>
2024-03-27 15:59:08 +0700

    update

--- src/app/component/base/components.module.ts
+++ src/app/component/base/components.module.ts
@@ -2,7 +2,6 @@ import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {PaginationComponent} from "../pagination/pagination.component";
-import {NzModalModule} from "ng-zorro-antd/modal";
 
 
 @NgModule({
@@ -15,13 +14,11 @@ import {NzModalModule} from "ng-zorro-antd/modal";
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    NzModalModule
   ],
   exports: [
     FormsModule,
     ReactiveFormsModule,
     PaginationComponent,
-    NzModalModule
   ],
   schemas: [NO_ERRORS_SCHEMA],
 })
