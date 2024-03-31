026d2ea279d463d7c1175be0b48079c3d151d94c
giangnt <giangnt@tecapro.com.vn>
2024-03-31 21:25:21 +0700

    update

--- src/app/pages/account/choose-nha-thuoc/choose-nha-thuoc.component.ts
+++ src/app/pages/account/choose-nha-thuoc/choose-nha-thuoc.component.ts
@@ -4,7 +4,7 @@ import {BaseComponent} from "../../../component/base/base.component";
 import {Router} from "@angular/router";
 import {SpinnerService} from "../../../services/spinner.service";
 import {NotificationService} from "../../../services/notification.service";
-import {MESSAGE} from "../../../constants/message";
+import {MESSAGE, STATUS_API} from "../../../constants/message";
 import {NhaThuocsService} from "../../../services/system/nha-thuocs.service";
 
 @Component({
@@ -21,13 +21,12 @@ export class ChooseNhaThuocComponent extends BaseComponent implements OnInit {
 
   ngOnInit() {
     this.formData = this.fb.group({
-      searchTen: [undefined],
+      textSearch: [undefined],
     });
     this.searchPage();
   }
 
   async chooseNhaThuoc() {
-    console.log('chooseNhaThuoc');
     if (this.nhaThuoc) {
       this.loadingService.show();
       let res = await this.authService.chooseNhaThuoc({id: this.nhaThuoc.id});
@@ -46,7 +45,32 @@ export class ChooseNhaThuocComponent extends BaseComponent implements OnInit {
   }
 
   chooseRow(data: any) {
-    console.log(data);
     this.nhaThuoc = data;
+  }
+
+  override async searchPage() {
+    await this.spinner.show();
+    try {
+      let body = this.formData.value
+      body.paggingReq = {
+        limit: this.pageSize,
+        page: this.page - 1
+      }
+      let res = await this.nhaThuocsService.searchPageNhaThuoc(body);
+      if (res?.statusCode == STATUS_API.SUCCESS) {
+        let data = res.data;
+        this.dataTable = data.content;
+        this.totalRecord = data.totalElements;
+        this.totalPages = data.totalPages;
+
+      } else {
+        this.dataTable = [];
+        this.totalRecord = 0;
+      }
+    } catch (e) {
+      this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+    } finally {
+      await this.spinner.hide();
+    }
   }
 }
--- src/app/services/system/nha-thuocs.service.ts
+++ src/app/services/system/nha-thuocs.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {BaseService} from "../base.service";
+import {ResponseData} from "../../models/response-data";
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,12 @@ import {BaseService} from "../base.service";
 export class NhaThuocsService extends BaseService {
 
   constructor(httpClient: HttpClient) {
-    super(httpClient, 'wnt-system','nha-thuocs');
+    super(httpClient, 'wnt-system', 'nha-thuocs');
   }
 
-
+  searchPageNhaThuoc(body: any) {
+    const url = `/api/wnt-system/nha-thuocs/search-page-nha-thuoc`;
+    return this.httpClient.post<ResponseData>(url, body).toPromise();
+  }
+  
 }
