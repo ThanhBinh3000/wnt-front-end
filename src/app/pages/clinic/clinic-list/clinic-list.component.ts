dfd23b55ad23e98f881ae0db0e472ddd34e2494c
haianh245 <tysunhb1234@gmail.com>
2024-04-10 15:59:55 +0700

    bác sĩ + phòng khám

--- src/app/pages/clinic/clinic-list/clinic-list.component.ts
+++ src/app/pages/clinic/clinic-list/clinic-list.component.ts
@@ -1,21 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { PhongKhamsService } from '../../../services/medical/phong-khams.service';
+import { BaseComponent } from '../../../component/base/base.component';
+import { MatSort } from '@angular/material/sort';
+import { ClinicAddEditDialogComponent } from '../clinic-add-edit-dialog/clinic-add-edit-dialog.component';
 
 @Component({
   selector: 'app-clinic-list',
   templateUrl: './clinic-list.component.html',
   styleUrl: './clinic-list.component.css'
 })
-export class ClinicListComponent implements OnInit {
+export class ClinicListComponent extends BaseComponent implements OnInit {
   title: string = "Danh sách phòng khám";
-  clinicID : number = 0;
+  displayedColumns = ['stt', 'tenPhongKham', 'description', 'action'];
   constructor(
-    private titleService: Title
+    injector: Injector,
+    private titleService: Title,
+    private _service: PhongKhamsService,
   ) {
+    super(injector, _service);
+    this.formData = this.fb.group({
+      tenPhongKham: [],
+    });
   }
+
+  @ViewChild(MatSort) sort?: MatSort;
 
   ngOnInit() {
     this.titleService.setTitle(this.title);
+    this.searchPage();
+  }
+
+  async ngAfterViewInit() {
+    this.dataSource.sort = this.sort!;
+  }
+
+  async openAddEditDialog(clinicID: any) {
+    const dialogRef = this.dialog.open(ClinicAddEditDialogComponent, {
+      data: clinicID,
+      width: '600px',
+    });
+    dialogRef.afterClosed().subscribe(async result => {
+      if (result) {
+        await this.searchPage();
+      }
+    });
   }
 
 }
--- src/app/pages/doctor/doctor-list/doctor-list.component.ts
+++ src/app/pages/doctor/doctor-list/doctor-list.component.ts
@@ -1,21 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Injector, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { BaseComponent } from '../../../component/base/base.component';
+import { BacSiesService } from '../../../services/medical/bac-sies.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { DoctorAddEditDialogComponent } from '../doctor-add-edit-dialog/doctor-add-edit-dialog.component';
 
 @Component({
   selector: 'doctor-list',
   templateUrl: './doctor-list.component.html',
   styleUrls: ['./doctor-list.component.css'],
 })
-export class DoctorListComponent implements OnInit {
+export class DoctorListComponent extends BaseComponent implements OnInit, AfterViewInit {
   title: string = "Danh sách bác sỹ";
-  doctorID: number = 0;
+  displayedColumns = ['#', 'tenBacSy', 'nhomBacSy', 'dienThoai', 'diaChi', 'active', 'action'];
 
   constructor(
-    private titleService: Title
+    injector: Injector,
+    private titleService: Title,
+    private _service: BacSiesService,
   ) {
+    super(injector, _service);
+    this.formData = this.fb.group({
+      tenBacSy: [],
+      dataDelete: [false]
+    });
   }
+
+  @ViewChild(MatSort) sort?: MatSort;
 
   ngOnInit() {
     this.titleService.setTitle(this.title);
+    this.searchPage();
+  }
+
+  async ngAfterViewInit() {
+    this.dataSource.sort = this.sort!;
+  }
+
+  async openAddEditDialog(doctorID: any) {
+    const dialogRef = this.dialog.open(DoctorAddEditDialogComponent, {
+      data: doctorID,
+      width: '600px',
+    });
+    dialogRef.afterClosed().subscribe(async result => {
+      if (result) {
+        await this.searchPage();
+      }
+    });
   }
 }
